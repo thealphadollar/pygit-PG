c3e066d5423bef385f71d26121d0025c0e8c378a
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-02-28 19:14:46 +0530

    add remote comparison functions

--- src/comp.py
+++ src/comp.py
@@ -1,6 +1,7 @@
 """
 contains all the methods to compare remote and local master branch.
 """
+import stat
 
 from .indexing import read_object
 
@@ -14,9 +15,9 @@ def read_tree(sha1=None, data=None):
     :param sha1: hex string, optional
     :param data: data of the tree object, defaults to None
     :param data: byte string, optional
-    :raises TypeError: [description]
-    :return: [description]
-    :rtype: [type]
+    :raises TypeError: when neither data nor sha1 string provided
+    :return: entries
+    :rtype: string
     """
 
     if sha1 is not None:
@@ -36,3 +37,68 @@ def read_tree(sha1=None, data=None):
         entries.append(mode, path, digest.hex())
         i = end+1+20
     return entries
+
+def find_tree_objects(tree_sha1):
+    """
+    find hashes of all objects
+    
+    return set of sha-1 hashes of all objects in this tree (recursively), including
+    the hash of the tree itself.
+    
+    :param tree_sha1: sha1 hash of the tree object
+    :type tree_sha1: hex string
+    :return: objects of the tree
+    :rtype: list
+    """
+
+    objects = {tree_sha1}
+    for mode, path, sha1 in read_tree(sha1=tree_sha1):
+        if stat.S_ISDIR(mode):
+            objects.update(find_tree_objects(sha1))
+        else:
+            objects.add(sha1)
+    return objects
+
+def find_commit_objects(commit_sha1):
+    """
+    find SHA-1 hashes of objects in the commit
+    
+    return set of SHA-1 hashes of all objects in this commit (recursively), 
+    it's frees, it's parents and the hash of the commit itself.
+    
+    :param commit_sha1: SHA-1 hash of the commit
+    :type commit_sha1: string
+    :return: SHA-1 hash of the objects present in the commit
+    :rtype: list
+    """
+    objects = {commit_sha1}
+    obj_type, commit = read_object(commit_sha1)
+    assert obj_type == 'commit'
+    lines = commit.decode().splitlines()
+    tree = next(l[5:45] for l in lines if l.startswith('tree '))
+    objects.update(find_tree_objects(tree))
+    parents = (l[7:47] for l in lines if l.startswith('parent '))
+    for parent in parents:
+        objects.update(find_commit_objects(parent))
+    return objects
+
+def find_missing_objects(local_sha1, remote_sha1):
+    """
+    find local objects not present in remote
+    
+    return set of SHA-1 hashes of objects in local commit that are missing
+    at the remote (based on the given remote commit hash).
+    
+    :param local_sha1: SHA-1 of local commit
+    :type local_sha1: string
+    :param remote_sha1: SHA-1 of remote commit
+    :type remote_sha1: string
+    :return: set of SHA-1 hashes of local objects not in remote
+    :rtype: list
+    """
+
+    local_objects = find_commit_objects(local_sha1)
+    if remote_sha1 is None:
+        return local_objects
+    remote_objects = find_commit_objects(remote_sha1)
+    return local_objects - remote_objects
